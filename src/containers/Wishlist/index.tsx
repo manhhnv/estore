a3fecd30cc7412aecf90ba395463213f3f44a696
minhglhf <minhtq30102000@gmail.com>
2021-04-14 08:12:47 +0700

    reducer

--- src/containers/Wishlist/index.tsx
+++ src/containers/Wishlist/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import { View, TouchableOpacity, Text } from 'react-native';
 import { useActiveWishlistQuery } from 'estore/graphql/generated';
 import GridPlaceholder from 'estore/components/templates/GridPlaceholder';
@@ -7,16 +7,26 @@ import { NavigationProp, useNavigation } from '@react-navigation/core';
 import { HomeStackParamList } from 'estore/types';
 import Grid from 'estore/components/ProductsList/Grid';
 import { UserSliceType } from 'estore/redux/slice/userSlice';
-import Login from 'estore/containers//Credential/Login';
+import { WishlistSliceType } from 'estore/redux/slice/wishlistSlice';
 import { RootState } from 'estore/redux/slice/index';
 import { connect } from 'react-redux';
+import { ActionCreatorWithPayload } from '@reduxjs/toolkit';
+
 type WishlistProps = {
     user: UserSliceType;
+    getWishlist: ActionCreatorWithPayload<WishlistSliceType, string>;
 };
 
-const Wishlist = ({ user}: WishlistProps) => {
+
+const Wishlist = ({ user, getWishlist}: WishlistProps) => {
     const { data, loading, error } = useActiveWishlistQuery();
-    console.log(data?.activeWishlist);
+     console.log(getWishlist)
+    useEffect(() => {
+        if (data && data?.activeWishlist) {
+            console.log(data)
+        }
+    }, [data]);
+
     const navigation = useNavigation<NavigationProp<HomeStackParamList>>();
     if (loading) {
         return <GridPlaceholder />;
@@ -36,7 +46,8 @@ const Wishlist = ({ user}: WishlistProps) => {
 };
 const mapStateToProps = (state: RootState) => {
     return {
-        user: state.user
+        user: state.user,
+        wishlist: state.wishlist
     };
 };
 const mapDispatchToProps = { };
--- src/redux/slice/wishlistSlice.ts
+++ src/redux/slice/wishlistSlice.ts
@@ -1,18 +1,30 @@
 import { createSlice } from '@reduxjs/toolkit';
 import { Product } from 'estore/graphql/generated';
 
-const initialState: Product[] = [];
+export type WishlistSliceType = {
+    total: number,
+    list: Product[]
+};
+
+const initialState: WishlistSliceType = {total: 0, list: []};
 const wishlistSlice = createSlice({
     name: 'wishlist',
     initialState: initialState,
     reducers: {
+        getWishlist: (state, action) => {
+            return state;
+        },
         addToWishlist: (state, action) => {
+            state.total = state.total + 1;
+            state.list.push(action.payload.data)
             return state;
         },
         removeFromWishlist: (state, action) => {
+            state.total = state.total - 1;
+            // state.list.pop(action.payload.data)
             return state;
         }
     }
 })
-export const { addToWishlist, removeFromWishlist } = wishlistSlice.actions;
+export const { getWishlist, addToWishlist, removeFromWishlist } = wishlistSlice.actions;
 export default wishlistSlice.reducer;
