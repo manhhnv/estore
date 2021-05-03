45d52d573991cdd7a3b414761a094f7f79ec52f8
Nguyen-Van-Manh <manh117bg@gmail.com>
2021-05-03 15:47:16 +0700

    upadte checkout

---
+++ src/containers/Checkout/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View } from 'react-native';
+import Cart from 'estore/containers/Cart';
+
+const Checkout = () => {
+    return (
+        <Cart />
+    )
+}
+
+export default React.memo(Checkout);
